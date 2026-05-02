13f1f1465735ca2fe38b85aba4e90e6f8de9db20
BM-Nafizur-Rahman <b.m.nafiz.r10@gmail.com>
2026-05-02 11:38:38 +0600

    Updated_Database

--- database/house_rent_db.sql
+++ database/house_rent_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2026 at 05:54 AM
+-- Generation Time: May 02, 2026 at 07:36 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,6 +34,20 @@ CREATE TABLE `agreement` (
   `first_month_rent` int(11) NOT NULL DEFAULT 0,
   `owner_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `agreement`
+--
+
+INSERT INTO `agreement` (`agreement_id`, `advance`, `start_date`, `first_month_rent`, `owner_id`) VALUES
+(1, 25000, '2026-05-01', 0, 4),
+(2, 2000, '2026-05-01', 0, 4),
+(3, 4000, '2026-05-01', 0, 4),
+(4, 9000, '2026-05-01', 0, 4),
+(5, 15000, '2026-05-02', 30000, 4),
+(6, 2000, '2026-05-02', 30000, 6),
+(7, 500, '2026-05-02', 1000, 6),
+(8, 500, '2026-05-02', 1000, 6);
 
 -- --------------------------------------------------------
 
@@ -76,7 +90,12 @@ INSERT INTO `flat` (`flat_id`, `area`, `location`, `asking_rent`, `bedroom`, `fl
 (2, 1200, 'Merul Badda', 20000, 3, 2, 'House No: 12, Road No: 16, DIT Project, Merul Badda, Dhaka', 'Available', 2),
 (3, 20000, 'Adabor', 30000, 3, 5, 'Adabor-1, Shompa Market', 'Available', 2),
 (4, 1000, 'Mohammadpur', 15000, 2, 5, 'Nurjahan Road, Mohammadpur', 'Available', 2),
-(5, 1200, 'Nakhalpara', 15000, 2, 6, 'Lucas Mor, Nakhalpara', 'Available', 2);
+(5, 1200, 'Nakhalpara', 15000, 2, 6, 'Lucas Mor, Nakhalpara', 'Available', 2),
+(6, 1200, 'Badda', 25000, 2, 4, 'Post Office Goli', 'Rented', 4),
+(7, 1300, 'Chottogram', 30000, 5, 5, 'Khulshi', 'Rented', 4),
+(8, 2000, 'Mohammadpur', 30000, 3, 4, 'Japan Garden', 'Rented', 6),
+(9, 12, 'Chottogram', 1000, 2, 4, 'ttttt', 'Rented', 6),
+(10, 3, 'Gulshan1', 1000, 4, 1, '3', 'Rented', 6);
 
 -- --------------------------------------------------------
 
@@ -89,6 +108,20 @@ CREATE TABLE `links` (
   `tenant_id` int(11) NOT NULL,
   `flat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `links`
+--
+
+INSERT INTO `links` (`agreement_id`, `tenant_id`, `flat_id`) VALUES
+(1, 5, 6),
+(2, 5, 6),
+(3, 5, 6),
+(4, 5, 6),
+(5, 5, 7),
+(6, 7, 8),
+(7, 5, 9),
+(8, 7, 10);
 
 -- --------------------------------------------------------
 
@@ -109,6 +142,21 @@ CREATE TABLE `monthly_bill` (
   `payment_status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `monthly_bill`
+--
+
+INSERT INTO `monthly_bill` (`agreement_id`, `billing_month`, `base_rent`, `maintanance`, `electricity`, `gas`, `water`, `service_charge`, `total_amount`, `payment_status`) VALUES
+(1, 'May-2026', 25000, 0, 0, 0, 0, 0, 25000, 'Paid'),
+(2, 'May-2026', 25000, 0, 0, 0, 0, 0, 25000, 'Paid'),
+(3, 'May-2026', 25000, 0, 0, 0, 0, 0, 25000, 'Paid'),
+(4, 'May-2026', 25000, 0, 500, 1200, 1000, 650, 28350, 'Paid'),
+(5, 'May-2026', 30000, 0, 0, 0, 0, 0, 30000, 'Paid'),
+(6, 'June-2026', 30000, 0, 800, 200, 400, 23, 31423, 'Paid'),
+(6, 'May-2026', 30000, 0, 700, 1200, 500, 200, 32600, 'Paid'),
+(7, 'May-2026', 1000, 0, 0, 0, 0, 0, 1000, 'Paid'),
+(8, 'May-2026', 1000, 0, 0, 0, 0, 0, 1000, 'Paid');
+
 -- --------------------------------------------------------
 
 --
@@ -128,7 +176,9 @@ CREATE TABLE `owner` (
 
 INSERT INTO `owner` (`user_id`, `bank_name`, `account_number`, `branch_name`) VALUES
 (2, 'Brac Bank', '125874963587', 'Mohakhali'),
-(3, 'Estern Bank', '522147932587458', 'Mohammadpur, Dhaka');
+(3, 'Estern Bank', '522147932587458', 'Mohammadpur, Dhaka'),
+(4, 'bbbb', 'bbbbb', 'bbbbb'),
+(6, 'Trust Bank', '63847484994', 'Mohammadpur');
 
 -- --------------------------------------------------------
 
@@ -160,6 +210,25 @@ CREATE TABLE `request` (
   `flat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `request`
+--
+
+INSERT INTO `request` (`request_id`, `date`, `request_status`, `offer_advance`, `offer_start_date`, `tenant_id`, `flat_id`) VALUES
+(1, '2026-05-02', 'Approved', NULL, NULL, 5, 6),
+(2, '2026-05-02', 'Approved', NULL, NULL, 5, 6),
+(3, '2026-05-02', 'Approved', NULL, NULL, 5, 6),
+(4, '2026-05-02', 'Approved', NULL, NULL, 5, 6),
+(5, '2026-05-02', 'Pending', NULL, NULL, 5, 5),
+(6, '2026-05-02', 'Approved', NULL, NULL, 5, 7),
+(7, '2026-05-02', 'Approved', NULL, NULL, 7, 8),
+(8, '2026-05-02', 'Pending', NULL, NULL, 7, 5),
+(9, '2026-05-02', 'Pending', NULL, NULL, 7, 3),
+(10, '2026-05-02', 'Pending', NULL, NULL, 7, 2),
+(11, '2026-05-02', 'Pending', NULL, NULL, 7, 1),
+(12, '2026-05-02', 'Approved', NULL, NULL, 5, 9),
+(13, '2026-05-02', 'Approved', 500.00, '2026-05-02', 7, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -177,7 +246,9 @@ CREATE TABLE `tenant` (
 --
 
 INSERT INTO `tenant` (`user_id`, `permanent_address`, `occupation`) VALUES
-(1, 'Dhaka', 'Student');
+(1, 'Dhaka', 'Student'),
+(5, 'Minanmar', 'Banker'),
+(7, 'Uttora', 'Businessman');
 
 -- --------------------------------------------------------
 
@@ -201,7 +272,11 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `name`, `nid`, `phone`, `email`, `password`) VALUES
 (1, 'Bodruzzaman', '1234567890', '01234567891', 'abc@abc.com', '$2y$10$QV/.wTLc5lqoYjqV7qrPfuoKJ6Z3P7sOEShMgMS6eHWdB3ZWvIrSG'),
 (2, 'Hasanuzzaman', '5412635871', '01254698724', 'def@def.com', '$2y$10$ZqFIumnxDjoilGz0vSD7/.aLQey1QrPo2.BL2RnAh9q0C26R40ezO'),
-(3, 'Nazrul Islam', '2541896357', '15789632874', 'nazrul@abc.com', '$2y$10$KZG7p/blLyDx7XQdhbAjgOBWo3ri0SzGWl8NkVEVt12m.9mmX90XW');
+(3, 'Nazrul Islam', '2541896357', '15789632874', 'nazrul@abc.com', '$2y$10$KZG7p/blLyDx7XQdhbAjgOBWo3ri0SzGWl8NkVEVt12m.9mmX90XW'),
+(4, 'Nafiz Rahman', '3245325345', '01859166886', 'ventral.fashion@gmail.com', '$2y$10$ebQqllekxxJeYEfxWzMNHuMTDx2FkYMTjHeoMVXskM4D6MLm0gsfK'),
+(5, 'Tanvir Hossain', '9409409877', '01341802359', 'tanvir.hossain@gmail.com', '$2y$10$iM/d/./EOflCDBKLi4gq3u9qAl6PBfN.zQ8K5xXmixE5KAmskJuD2'),
+(6, 'Saba', '7383684848', '01938847484', 'saba@gmail.com', '$2y$10$6RideR/neTfBpBTxJ4lwBOcn9SyH3piDcy9MGheCNANFZ55Z6tShG'),
+(7, 'Asad', '7386246234', '012638384', 'asadpagal@gmail.com', '$2y$10$wWlQGKpb6LwuvHu/JdVrdObKT5hrFJeKn9KjgMV4O.tqZkxDxk/JC');
 
 --
 -- Indexes for dumped tables
@@ -286,7 +361,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `agreement`
 --
 ALTER TABLE `agreement`
-  MODIFY `agreement_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `agreement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `complaint`
@@ -298,7 +373,7 @@ ALTER TABLE `complaint`
 -- AUTO_INCREMENT for table `flat`
 --
 ALTER TABLE `flat`
-  MODIFY `flat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `flat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `rating`
@@ -310,13 +385,13 @@ ALTER TABLE `rating`
 -- AUTO_INCREMENT for table `request`
 --
 ALTER TABLE `request`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
